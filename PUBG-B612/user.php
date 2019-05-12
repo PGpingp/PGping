96c22a07e6d9ab7c74bfb42b80119e2d5c72f5de
tmvhsm4097 <32450742+tmvhsm4097@users.noreply.github.com>
2019-05-13 00:04:18 +0900

    Update user.php

--- PUBG-B612/user.php
+++ PUBG-B612/user.php
@@ -1,2 +1,2 @@
-n[Sull E]ni|192.168.219.165|i
-n[Wheneveryoucall#8718]ni|192.168.35.70|i
+n[Sull E]ni|192.168.219.165|im[test@naver.com]mc[RZDPOCI9P6C6IVAVBP1G]c
+n[Wheneveryoucall#8718]ni|192.168.35.70|im[test@naver.com]mc[VN6VTE2D8FW8Q1CBDABS]c
